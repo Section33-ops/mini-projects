4f8781fa41d6a05bdefec093ff0c03ab13b475e2
Section33-ops <saeedwumbei33@gmail.com>
2025-09-22 06:55:24 -0400

    Add to-do list app project file

--- to-do-list-app/to_do_list_app.py
+++ to-do-list-app/to_do_list_app.py
@@ -1,1 +1,26 @@
 # 09/21/2025
+tasks = []
+
+print("Welcome!")
+while True:
+    menu = input("To-do List Menu: \n1. Add task \n2. Remove task \n3. View tasks \n4. Quit \nChoose: ")
+
+    if menu == "1":
+        add_task = input("Add task: ")
+        tasks.append(add_task)
+        pass
+
+    elif menu == "2":
+        remove_task = input("Remove task: ")
+        tasks.remove(remove_task)
+        pass
+    elif menu == "3":
+        for task in tasks:
+            print(f"- {task}")
+        pass
+    elif menu == "4":
+        quit()
+        pass
+    
+    else:
+        print("Invalid option!")
