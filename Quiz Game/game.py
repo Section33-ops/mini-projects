3d15fe9d627ea628e7e7003bd6abbc51e40a27f1
Section33-ops <saeedwumbei33@gmail.com>
2025-10-02 07:15:35 -0400

    Add score systme

--- Quiz Game/game.py
+++ Quiz Game/game.py
@@ -2,6 +2,8 @@
 
 from questions import questions
 
+score = 0
+
 for question in questions:
     print(question["question"])
     for choice in question["choices"]:
@@ -9,5 +11,7 @@ for question in questions:
     user_answer = input("Your answer: ").upper()
     if user_answer == question["answer"]:
         print("Correct!")
+        score += 1
     else:
         print("Wrong!")
+print(f"Your score is {score}")
