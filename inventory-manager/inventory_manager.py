be05d25e59f7047765c6080a1538f3f7c0db05cc
Section33-ops <saeedwumbei33@gmail.com>
2025-10-20 07:21:09 -0400

    Add method to add items to the item list and also method to display items. Also updated init method

--- inventory-manager/inventory_manager.py
+++ inventory-manager/inventory_manager.py
@@ -1,3 +1,21 @@
-class InventoryManager:
-    def __init__(self,  items):
-        self.items = items
+class InventoryManager: # Define class that manages items in inventory
+    def __init__(self,  items=None): # Initialize with a list of items
+        if items is None:   # If there is no item list
+            self.items = [] # Start with an empty list
+        else:   # Else
+            self.items = items  # Use existing list
+
+    def add_item(self): # Function to add item to item list
+        item = input("What item do you want to add? ")  #   Ask user for item to add to list
+        self.items.append(item) # Append item to item list
+        print(f"{item} added successfully") # Priny item added successfully
+    
+    def display_items(self):    # Function to display items
+        for item in self.items: # Loop through each item in the item list
+            print(item) # Print item
+
+
+manager = InventoryManager()
+manager.add_item()
+manager.add_item()
+manager.display_items()
