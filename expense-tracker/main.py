f4fb9e2faeec96fc2a6947f72bc1e5fa8db399f3
Section33-ops <saeedwumbei33@gmail.com>
2025-10-28 11:47:35 -0400

    Update view_total_option() function. It can now show the user their total expense. Also fixed the total_cost rounding to 2 decimal places

--- expense-tracker/main.py
+++ expense-tracker/main.py
@@ -6,7 +6,7 @@ def add_expense_option():
     item = input("What did you buy? ")
     item_price = float(input("How much did it cost? "))
     item_quantity = int(input("How many did you buy? "))
-    total_cost = round(item_price * item_quantity,)
+    total_cost = round(item_price * item_quantity, 2)
     item_expense = (item, item_price, item_quantity, total_cost)
     expenses.append(item_expense)
 
@@ -15,7 +15,15 @@ def view_expenses_option():
         print(f"Item: {expense[0]}, Price: {expense[1]}, Quantity: {expense[2]}, Total cost: {expense[3]}")
 
 def view_total_option():
-    pass
+    total = 0
+    if not expenses:
+        print("No expenses yet.")
+        return
+    else:
+        for expense in expenses:
+            total += expense[3]
+    print(f"Total spent so far is ${total}")
+    
 
 def menu():
     print("Welcome to the Expense Tracker! \n1. Add expense \n2. View expenses \n3. View total \n4. Exit")
@@ -26,7 +34,7 @@ def menu():
     elif option == "2":
         view_expenses_option()
     elif option == "3":
-        pass
+        view_total_option()
     elif option == "4":
         exit()
     else:
