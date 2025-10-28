2c0641d73d0afe6a147531c052ce70f6319ff490
Section33-ops <saeedwumbei33@gmail.com>
2025-10-28 11:22:14 -0400

    Add code to add and view expenses

--- expense-tracker/main.py
+++ expense-tracker/main.py
@@ -1,15 +1,20 @@
 # 10/27/2025
 
+expenses = []
+
 def add_expense_option():
-    pass
+    item = input("What did you buy? ")
+    item_price = float(input("How much did it cost? "))
+    item_quantity = int(input("How many did you buy? "))
+    total_cost = round(item_price * item_quantity,)
+    item_expense = (item, item_price, item_quantity, total_cost)
+    expenses.append(item_expense)
 
 def view_expenses_option():
-    pass
+    for expense in expenses:
+        print(f"Item: {expense[0]}, Price: {expense[1]}, Quantity: {expense[2]}, Total cost: {expense[3]}")
 
 def view_total_option():
-    pass
-
-def add_expense_option():
     pass
 
 def menu():
@@ -17,9 +22,9 @@ def menu():
     option = input("Choose an option: ")
 
     if option == "1":
-        pass
+        add_expense_option()
     elif option == "2":
-        pass
+        view_expenses_option()
     elif option == "3":
         pass
     elif option == "4":
