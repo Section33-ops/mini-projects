cdc88917b02abcd6b760e503b982a652894afeff
Section33-ops <saeedwumbei33@gmail.com>
2025-10-28 07:16:16 -0400

    Code menu layout

---
+++ expense-tracker/main.py
@@ -0,0 +1,32 @@
+# 10/27/2025
+
+def add_expense_option():
+    pass
+
+def view_expenses_option():
+    pass
+
+def view_total_option():
+    pass
+
+def add_expense_option():
+    pass
+
+def menu():
+    print("Welcome to the Expense Tracker! \n1. Add expense \n2. View expenses \n3. View total \n4. Exit")
+    option = input("Choose an option: ")
+
+    if option == "1":
+        pass
+    elif option == "2":
+        pass
+    elif option == "3":
+        pass
+    elif option == "4":
+        exit()
+    else:
+        print("You must enter a number for the option")
+
+while True:
+    menu()
+    
