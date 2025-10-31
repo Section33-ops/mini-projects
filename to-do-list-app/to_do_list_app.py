efc7d973cc0ed513f9a2384bf4b769c79ebafddd
Section33-ops <saeedwumbei33@gmail.com>
2025-10-31 07:54:52 -0400

    Explain lines of code with comments

--- to-do-list-app/to_do_list_app.py
+++ to-do-list-app/to_do_list_app.py
@@ -1,6 +1,6 @@
 # 09/21/2025
-import os
-import csv
+import os   # Import the os module
+import csv  # Import the csv module
 
 tasks = []
 
@@ -18,46 +18,47 @@ def load_tasks():
             return [row[0] for row in reader]  # Return tasks as a list of strings
     return []  # Return empty list if no tasks exist yet
 
-def delete_tasks(delete_task):
-    if delete_task in tasks:
-        tasks.remove(delete_task)
-        print(f"Removed: {delete_task}")
-        save_tasks(tasks)
-    else:
-        print("Task not found!")
+def delete_tasks(delete_task):  # Function to remove tasks
+    if delete_task in tasks:    # Checks if the task the user wants to delete is an existing task
+        tasks.remove(delete_task)   # Remove task from the tasks list
+        print(f"Removed: {delete_task}")    # Print Removed: task
+        save_tasks(tasks)   # Save the remaining tasks to the tasks file
+    else:   # If the task the user wants to delete does not exist
+        print("Task not found!")    # Print "Task not found!"
 
-print("Welcome!")
+print("Welcome!")   # Print "Welcome!"
 
-tasks = load_tasks()
+tasks = load_tasks()    # Loads tasks from file
 
-while True:
-    os.system('cls' if os.name == 'nt' else 'clear')
+while True: # While True
+    os.system('cls' if os.name == 'nt' else 'clear')    # Refreshes the terminal after evry action
 
-    menu = input("To-do List Menu: \n1. Add task \n2. Remove task \n3. View tasks \n4. Quit \nChoose: ")
+    menu = input("To-do List Menu: \n1. Add task \n2. Remove task \n3. View tasks \n4. Quit \nChoose: ")    # Prints options to user and prompts them to choose
 
-    if menu == "1":
-        add_task = input("Add task: ")
-        tasks.append(add_task)
-        save_tasks(add_task)
+    if menu == "1": # If user entered option "1"
+        add_task = input("Add task: ")  # Ask user for task to add
+        tasks.append(add_task)  # Append user's task to tasks list
+        save_tasks(add_task)    # Save user's task to tasks.csv
         
-    elif menu == "2":
-        remove_task = input("Remove task: ")
-        delete_tasks(remove_task)
+    elif menu == "2":   # If user entered option "2"
+        remove_task = input("Remove task: ")    # Ask user for task to remove
+        delete_tasks(remove_task)   # Remove task from tasks.csv
         
-    elif menu == "3":
-        if tasks:
-            print("Your tasks: ")
-            for task in tasks:
-                print(f"- {task}")
-        else:
-            print("No tasks yet!")
+    elif menu == "3":   # If user entered option "3"
+        if tasks:   # If there are tasks
+            print("Your tasks: ")   # Print "Your tasks: "
+            for task in tasks:  # Loop through each task in tasks list
+                print(f"- {task}")  # Print each task
+        else:   # If there no tasks
+            print("No tasks yet!")  # Print "No tasks yet!"
         
-    elif menu == "4":
-        print("Goodbye!")
-        break
+    elif menu == "4":   # If user entered option "4"
+        print("Goodbye!")   # Print "Goodbye!"
+        break   # Break out of the loop which exists the program
         
     
-    else:
-        print("Invalid option!")
+    else:   # If user entered wrong option
+        print("Invalid option!")    # Print "Invalid option!"
     
-    input("\nPress Enter to continue...")
+    input("\nPress Enter to continue...")   # Asks user to press ENTER to continue. This is used to paused the loop
+                                            # so the user has time to read the current terminal before it is cleared
